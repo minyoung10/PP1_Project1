c3015920e0a162a5c3e3cdbe6436f50b5e7357e0
minyoung10 <22000374@handong.ac.kr>
2023-09-08 16:15:07 +0900

    modify files

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -1,2 +1,53 @@
-package com.mycom.word;public class WordCRUD {
+package com.mycom.word;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCRUD implements ICRUD {
+    ArrayList<Word> list;
+    Scanner s;
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
+
+    @Override
+    public Object add() {
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level =s.nextInt();
+        String word = s.next();
+        s.nextLine(); // 엔터를 소비하기 위해 추가
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+        return new Word(0,level,word,meaning);
+    }
+    public void addWord(){
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다. ");
+    }
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+
+    public void listAll(){
+        System.out.println("-------------------------------");
+        for (int i=0; i< list.size(); i++){
+            System.out.print((i+1)+" ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("-------------------------------");
+    }
 }
